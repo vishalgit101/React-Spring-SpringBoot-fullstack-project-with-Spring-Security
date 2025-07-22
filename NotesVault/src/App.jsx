900f1796c12aaa4df951d6dded92f39353e97900
Vishal Choudhary <vishchy111@gmail.com>
2025-07-22 17:15:51 +0530

    Project setup for React Frontend. Added Dependencies and more

---
+++ NotesVault/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main>
+      <h1>This is inside App</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  );
+}
